697fd24902f5949318631cdcf1e52b4499045469
Igor Henrique Diniz Souza <igorsouza_datum@getnet.com.br>
2022-06-03 17:10:36 -0300

    Iniciando a Automação para a empresa WEBJUMP

---
+++ cypress/e2e/integration/test.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('Teste Cypress', () => {
+    it('visitando a pagina', () => {
+        cy.visit('https://wj-qa-automation-test.github.io/qa-test/')
+    })
+})

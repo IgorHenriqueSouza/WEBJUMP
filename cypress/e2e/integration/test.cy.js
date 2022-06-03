275617051cc82b268e81ec99124c7ec7a3c2fabb
Igor Henrique Diniz Souza <igorsouza_datum@getnet.com.br>
2022-06-03 20:57:40 -0300

    Fazendo o teste numero 3

--- cypress/e2e/integration/test.cy.js
+++ cypress/e2e/integration/test.cy.js
@@ -5,19 +5,23 @@ context('Teste Cypress WEBJUMP', () => {
         cy.visit('https://wj-qa-automation-test.github.io/qa-test/')
     })
 
-    it('Acessando o site e clicando em One, Two, Four', () => {
+    //Caso de teste 01: Acessando o site e clicando no botão One, Two e no link Four e realizando a validação de que os botões não se encontram mais na pagina
+
+    it('Teste1', () => {
         cy.get('#btn_one').click()
         cy.get('#btn_two').click()
         cy.get('#btn_link').click()
 
         cy.contains('#btn_one','#btn_two','#btn_link').should('not.exist')
     })
-    it('Acessando o site e clicando em One, Two, Four', () => {
-        cy.get('#btn_one').click()
-        cy.get('#btn_two').click()
-        cy.get('#btn_link').click()
 
-        cy.contains('#btn_one','#btn_two','#btn_link').should('not.exist')
+    //Caso de teste 03:Preenchendo o campo YouFirstName, clicando no botão One, dando um check na opção OptionThree, selecionando a opção ExemploTwo e validando a presença da imagem do logo Selenium Webdriver
+    it('Teste3', () => {
+      cy.get('#first_name').focus().type('WEBJUMP UHULLL!!')
+      cy.get('#btn_one').click()
+      cy.get('#opt_three').check()
+      cy.get('#select_box').select('ExampleTwo')
+      cy.get('[alt="selenium"]').should('be.visible')
     })
     
 })

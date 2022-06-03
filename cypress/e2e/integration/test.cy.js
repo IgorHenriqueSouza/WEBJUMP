8c423fdf4ae8efe9b937f0bb3c5e64a03990c802
Igor Henrique Diniz Souza <igorsouza_datum@getnet.com.br>
2022-06-03 17:35:20 -0300

    Implementando o primeiro teste

--- cypress/e2e/integration/test.cy.js
+++ cypress/e2e/integration/test.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Teste Cypress', () => {
-    it('visitando a pagina', () => {
+context('Teste Cypress WEBJUMP', () => {
+    beforeEach(() => {
         cy.visit('https://wj-qa-automation-test.github.io/qa-test/')
     })
+
+    it('Acessando o site e clicando em One, Two, Four', () => {
+        cy.get('#btn_one').click()
+        cy.get('#btn_two').click()
+        cy.get('#btn_link').click()
+
+        cy.contains('#btn_one','#btn_two','#btn_link').should('not.exist')
+    })
+    it('Acessando o site e clicando em One, Two, Four', () => {
+        cy.get('#btn_one').click()
+        cy.get('#btn_two').click()
+        cy.get('#btn_link').click()
+
+        cy.contains('#btn_one','#btn_two','#btn_link').should('not.exist')
+    })
+    
 })
